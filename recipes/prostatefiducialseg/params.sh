0c0ffcdac34aee612421c7b2f383f17a223b0da5
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-07-15 17:18:51 +1000

    rebuild

--- recipes/prostatefiducialseg/params.sh
+++ recipes/prostatefiducialseg/params.sh
@@ -2,4 +2,5 @@
 # build image here: https://github.com/NeuroDesk/neurocontainers and add mrd server instructions: https://www.neurodesk.org/docs/getting-started/neurocontainers/openrecon/
 # specify the repostiory and name of the docker image: https://hub.docker.com/orgs/vnmd/repositories
 export version=3.0.0
+# rebuild
 export baseDockerImage=vnmd/prostatefiducialseg_${version}
