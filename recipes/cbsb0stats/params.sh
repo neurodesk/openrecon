4cce13e72b428916ba57149a97a17fb8df78dfe5
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-11-13 09:02:48 -0800

    build

--- recipes/cbsb0stats/params.sh
+++ recipes/cbsb0stats/params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # specify the repostiory and name of the docker image
 export toolName=cbsb0stats
-export version=1.1.1
+export version=1.1.0
 export baseDockerImage=vnmd/${toolName}_${version}
