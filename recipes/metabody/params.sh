9199156dba4a61a1c8a3335091eb4753b579f93d
Steffen Bollmann <stebo85@users.noreply.github.com>
2026-02-04 15:36:14 -0800

    rebuild

--- recipes/metabody/params.sh
+++ recipes/metabody/params.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # build image here: https://github.com/NeuroDesk/neurocontainers and add mrd server instructions: https://www.neurodesk.org/docs/getting-started/neurocontainers/openrecon/
-# specify the repostiory and name of the docker image: https://hub.docker.com/orgs/vnmd/repositories
 export toolName=metabody
 export version=1.0.1
 export baseDockerImage=vnmd/${toolName}_${version}
