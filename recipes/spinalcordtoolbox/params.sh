72be189425ae207f1cd8cd4480e98866a4af74ea
Steffen Bollmann <stebo85@users.noreply.github.com>
2026-06-04 20:03:00 -0700

    remove openrecon version

--- recipes/spinalcordtoolbox/params.sh
+++ recipes/spinalcordtoolbox/params.sh
@@ -2,9 +2,8 @@
 # build image here: https://github.com/NeuroDesk/neurocontainers and add mrd server instructions: https://www.neurodesk.org/docs/getting-started/neurocontainers/openrecon/
 # specify the repostiory and name of the docker image: https://hub.docker.com/orgs/vnmd/repositories
 export toolName=spinalcordtoolbox
-export version=7.1
+export version=7.1.1
 export baseDockerImage=vnmd/${toolName}_${version}
-export openrecon_version=${version}.15 #this is only necessary if tool version is not following semantic versioning
 # this image is build based on 
 # https://github.com/neurodesk/neurocontainers/blob/main/recipes/spinalcordtoolbox/build.yaml
 # source .venv/bin/activate
