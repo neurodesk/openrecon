515bb96a5af1f2348ce1c057994c81136713c33b
Steffen Bollmann <stebo85@users.noreply.github.com>
2026-09-02 15:25:23 -0700

    skipci

--- recipes/metabody/params.sh
+++ recipes/metabody/params.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # build image here: https://github.com/NeuroDesk/neurocontainers and add mrd server instructions: https://www.neurodesk.org/docs/getting-started/neurocontainers/openrecon/
 export toolName=metabody
-export version=1.0.3
+export version=1.0.4
 export baseDockerImage=vnmd/${toolName}_${version}
 # this image is build based on 
 # https://github.com/neurodesk/neurocontainers/blob/main/recipes/metabody/build.yaml
+# source .venv/bin/activate
+# cd recipes/metabody
+# /bin/bash ../build.sh --local-cache
